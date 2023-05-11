0ac91f04413cb838988ebbd490003138e914f9f8
Krxk <1246924370@qq.com>
2023-05-12 01:04:42 +0800

    second_version

---
+++ ExWeight.py
@@ -0,0 +1,21 @@
+# coding=utf-8
+"""
+补充权重：根据用户行为改变
+"""
+import numpy as np
+import User
+
+# 观看历史类别权重：
+
+class ExtraWeight:
+    def __init__(self):
+        self.exWeightList = np.empty(10, dtype=float)
+
+    def genExweight(self, user: User.User):
+        for category in range(10):
+            self.exWeightList[category] = user.stay_p(category)
+            self.exWeightList /= self.exWeightList.sum()
+
+
+
+
