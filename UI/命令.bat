484de3bc3316238ea29f60a25285c98c0873e95d
Krxk <1246924370@qq.com>
2023-05-16 01:51:21 +0800

    version_3

---
+++ UI/命令.bat
@@ -0,0 +1,5 @@
+python -m PyQt5.uic.pyuic user.ui -o uiuser.py
+python -m PyQt5.uic.pyuic userlogin.ui -o uiuserlogin.py
+python -m PyQt5.uic.pyuic video.ui -o uivideo.py
+python -m PyQt5.uic.pyuic videologin.ui -o uivideologin.py
+python -m PyQt5.uic.pyuic widget.ui -o uiwidget.py
