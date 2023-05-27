bf482847418b4ef0690f38c9d39ac871f991c853
Krxk <1246924370@qq.com>
2023-05-28 02:46:26 +0800

    version_release

--- UI/生成命令.bat
+++ UI/生成命令.bat
@@ -5,3 +5,4 @@ python -m PyQt5.uic.pyuic AnalysisWnd.ui -o AnalysisWnd.py
 python -m PyQt5.uic.pyuic MainWnd.ui -o MainWnd.py
 python -m PyQt5.uic.pyuic VideoLoginWnd.ui -o VideoLoginWnd.py
 python -m PyQt5.uic.pyuic UserLoginWnd.ui -o UserLoginWnd.py
+python -m PyQt5.uic.pyuic welcome.ui -o welcome.py
